713216219edb992a3ff5652875e60b6fa413bf25
DawidMaron <daw.maron@gmail.com>
2025-06-02 23:28:09 +0200

    fix -add repaint

--- src/main/java/com/maron/dawid/inputs/KeyboardInputs.java
+++ src/main/java/com/maron/dawid/inputs/KeyboardInputs.java
@@ -18,11 +18,21 @@ public class KeyboardInputs implements KeyListener {
     @Override
     public void keyPressed(KeyEvent e) {
         switch(e.getKeyCode()) {
-            case KeyEvent.VK_W -> gamePanel.changeYDelta(-5);
-            case KeyEvent.VK_S -> gamePanel.changeYDelta(5);
-            case KeyEvent.VK_A -> gamePanel.changeXDelta(-5);
-            case KeyEvent.VK_D -> gamePanel.changeXDelta(5);
+            case KeyEvent.VK_W -> {
+                gamePanel.changeYDelta(-5);
+            }
+            case KeyEvent.VK_S -> {
+                gamePanel.changeYDelta(5);
+            }
+            case KeyEvent.VK_A -> {
+                gamePanel.changeXDelta(-5);
+            }
+            case KeyEvent.VK_D -> {
+                gamePanel.changeXDelta(5);
+            }
         }
+        gamePanel.repaint();
+
     }
 
     @Override
