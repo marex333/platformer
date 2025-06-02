7f017f9b2ec79f100d7c88db267235de2174ce12
DawidMaron <daw.maron@gmail.com>
2025-06-02 23:26:23 +0200

    configure inputs

--- src/main/java/com/maron/dawid/main/GamePanel.java
+++ src/main/java/com/maron/dawid/main/GamePanel.java
@@ -1,20 +1,35 @@
 package com.maron.dawid.main;
 
 import com.maron.dawid.inputs.KeyboardInputs;
+import com.maron.dawid.inputs.MouseInputs;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class GamePanel extends JPanel {
+    private MouseInputs mouseInputs;
+    private int xDelta = 0;
+    private int yDelta = 0;
 
     public GamePanel() {
-        addKeyListener(new KeyboardInputs());
+        mouseInputs = new MouseInputs();
+        addKeyListener(new KeyboardInputs(this));
+        addMouseListener(mouseInputs);
+        addMouseMotionListener(mouseInputs);
+    }
+
+    public void changeXDelta(int value) {
+        this.xDelta += value;
+    }
+
+    public void changeYDelta(int value) {
+        this.yDelta += value;
     }
 
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        g.fillRect(100, 100, 200, 50);
+        g.fillRect(100 + xDelta, 100 + yDelta, 200, 50);
     }
 }
