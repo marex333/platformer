7695eafa1ace31967ef9602d145b653f2f874351
DawidMaron <daw.maron@gmail.com>
2025-06-03 14:50:27 +0200

    refactor game dimensions

--- src/main/java/com/maron/dawid/main/Game.java
+++ src/main/java/com/maron/dawid/main/Game.java
@@ -3,12 +3,18 @@ package com.maron.dawid.main;
 import com.maron.dawid.entities.Player;
 
 import java.awt.*;
-import java.awt.event.WindowEvent;
 
 public class Game implements Runnable {
 
     private final int FPS_SET = 120;
     private final int UPS_SET = 200;
+    public final static int TILES_DEFAULT_SIZE = 32;
+    public final static float SCALE = 1.5f;
+    public final static int TILES_IN_WIDTH = 26;
+    public final static int TILES_IN_HEIGHT = 14;
+    public final static int TILES_SIZE = (int)(TILES_DEFAULT_SIZE * SCALE);
+    public final static int GAME_WIDTH = TILES_SIZE * TILES_IN_WIDTH;
+    public final static int GAME_HEIGHT = TILES_SIZE * TILES_IN_HEIGHT;
 
     private GameWindow gameWindow;
     private GamePanel gamePanel;
--- src/main/java/com/maron/dawid/main/GamePanel.java
+++ src/main/java/com/maron/dawid/main/GamePanel.java
@@ -6,6 +6,9 @@ import com.maron.dawid.inputs.MouseInputs;
 import javax.swing.*;
 import java.awt.*;
 
+import static com.maron.dawid.main.Game.GAME_HEIGHT;
+import static com.maron.dawid.main.Game.GAME_WIDTH;
+
 public class GamePanel extends JPanel {
     private Game game;
     private MouseInputs mouseInputs;
@@ -20,8 +23,9 @@ public class GamePanel extends JPanel {
     }
 
     private void setPanelSize() {
-        Dimension size = new Dimension(1280, 720);
+        Dimension size = new Dimension(GAME_WIDTH, GAME_HEIGHT);
         setPreferredSize(size);
+        System.out.printf("size: %s : %s\n", GAME_WIDTH, GAME_HEIGHT);
     }
 
     @Override
