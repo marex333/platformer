58d426e6be70f50463ac7941a604e10806e0e5f3
DawidMaron <daw.maron@gmail.com>
2025-06-03 10:02:37 +0200

    cleanup

--- src/main/java/com/maron/dawid/main/GamePanel.java
+++ src/main/java/com/maron/dawid/main/GamePanel.java
@@ -5,31 +5,35 @@ import com.maron.dawid.inputs.MouseInputs;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.Random;
 
 public class GamePanel extends JPanel {
     private MouseInputs mouseInputs;
     private float xDelta = 100, yDelta = 100;
-    private float xDir = 0.25f, yDir = 0.25f;
-    private Color rectangleColor = new Color(50, 115, 140);
-    private Random random;
 
     public GamePanel() {
+        setPanelSize();
         mouseInputs = new MouseInputs(this);
         addKeyListener(new KeyboardInputs(this));
         addMouseListener(mouseInputs);
         addMouseMotionListener(mouseInputs);
-        random = new Random();
     }
 
+    private void setPanelSize() {
+        Dimension size = new Dimension(1280, 720);
+        setPreferredSize(size);
+    }
+
+    // for keyboard
     public void changeXDelta(int value) {
         this.xDelta += value;
     }
 
+    // for keyboard
     public void changeYDelta(int value) {
         this.yDelta += value;
     }
 
+    // for mouse
     public void setRectPos(int x, int y) {
         this.xDelta = x;
         this.yDelta = y;
@@ -38,26 +42,5 @@ public class GamePanel extends JPanel {
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-
-        updateRectangle();
-        g.setColor(rectangleColor);
-        g.fillRect((int)xDelta, (int)yDelta, 200, 50);
-    }
-
-    private void updateRectangle() {
-        xDelta += xDir;
-        if (xDelta > 400 || xDelta < 0) {
-            xDir = -xDir;
-            rectangleColor = getRandomColor();
-        }
-        yDelta += yDir;
-        if (yDelta > 400 || yDelta < 0) {
-            yDir = -yDir;
-            rectangleColor = getRandomColor();
-        }
-    }
-
-    private Color getRandomColor() {
-        return new Color(random.nextInt(256), random.nextInt(256), random.nextInt(256));
     }
 }
--- src/main/java/com/maron/dawid/main/GameWindow.java
+++ src/main/java/com/maron/dawid/main/GameWindow.java
@@ -8,11 +8,13 @@ public class GameWindow {
     public GameWindow(GamePanel gamePanel) {
         jFrame = new JFrame();
 
-        jFrame.setSize(400, 400);
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jFrame.add(gamePanel);
         jFrame.setLocationRelativeTo(null);
+        jFrame.setResizable(false);
+        jFrame.pack();
         jFrame.setVisible(true);
+
 
     }
 }
