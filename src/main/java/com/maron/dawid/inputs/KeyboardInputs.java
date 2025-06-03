5538bd945be3881589def5119a302b2a0a855c39
DawidMaron <daw.maron@gmail.com>
2025-06-03 14:30:43 +0200

    feature/003-add_player_actions

--- src/main/java/com/maron/dawid/inputs/KeyboardInputs.java
+++ src/main/java/com/maron/dawid/inputs/KeyboardInputs.java
@@ -1,7 +1,6 @@
 package com.maron.dawid.inputs;
 
 import com.maron.dawid.main.GamePanel;
-import com.maron.dawid.utils.Constants.Direction;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -18,23 +17,12 @@ public class KeyboardInputs implements KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
+        var player =  gamePanel.getGame().getPlayer();
         switch(e.getKeyCode()) {
-            case KeyEvent.VK_W -> {
-                gamePanel.changeYDelta(-5);
-                gamePanel.setDirection(Direction.UP);
-            }
-            case KeyEvent.VK_S -> {
-                gamePanel.changeYDelta(5);
-                gamePanel.setDirection(Direction.DOWN);
-            }
-            case KeyEvent.VK_A -> {
-                gamePanel.changeXDelta(-5);
-                gamePanel.setDirection(Direction.LEFT);
-            }
-            case KeyEvent.VK_D -> {
-                gamePanel.changeXDelta(5);
-                gamePanel.setDirection(Direction.RIGHT);
-            }
+            case KeyEvent.VK_W -> player.setUp(true);
+            case KeyEvent.VK_S -> player.setDown(true);
+            case KeyEvent.VK_A -> player.setLeft(true);
+            case KeyEvent.VK_D -> player.setRight(true);
         }
         gamePanel.repaint();
 
@@ -42,12 +30,12 @@ public class KeyboardInputs implements KeyListener {
 
     @Override
     public void keyReleased(KeyEvent e) {
+        var player =  gamePanel.getGame().getPlayer();
         switch(e.getKeyCode()) {
-            case KeyEvent.VK_W -> {}
-            case KeyEvent.VK_S -> {}
-            case KeyEvent.VK_A -> {}
-            case KeyEvent.VK_D -> {}
+            case KeyEvent.VK_W -> player.setUp(false);
+            case KeyEvent.VK_S -> player.setDown(false);
+            case KeyEvent.VK_A -> player.setLeft(false);
+            case KeyEvent.VK_D -> player.setRight(false);
         }
-        gamePanel.setMoving(false);
     }
 }
