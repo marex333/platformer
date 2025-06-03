6a573c356e738dec326fdd9201ee4c92eb8318f3
DawidMaron <daw.maron@gmail.com>
2025-06-03 08:51:16 +0200

    add moving rectangle with color switch add fps counter

--- src/main/java/com/maron/dawid/main/GamePanel.java
+++ src/main/java/com/maron/dawid/main/GamePanel.java
@@ -5,17 +5,23 @@ import com.maron.dawid.inputs.MouseInputs;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Random;
 
 public class GamePanel extends JPanel {
     private MouseInputs mouseInputs;
-    private int xDelta = 100;
-    private int yDelta = 100;
+    private float xDelta = 100, yDelta = 100;
+    private float xDir = 0.15f, yDir = 0.15f;
+    private int frames = 0;
+    private long lastCheck = 0;
+    private Color rectangleColor = new Color(50, 115, 140);
+    private Random random;
 
     public GamePanel() {
         mouseInputs = new MouseInputs(this);
         addKeyListener(new KeyboardInputs(this));
         addMouseListener(mouseInputs);
         addMouseMotionListener(mouseInputs);
+        random = new Random();
     }
 
     public void changeXDelta(int value) {
@@ -35,6 +41,34 @@ public class GamePanel extends JPanel {
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        g.fillRect(xDelta, yDelta, 200, 50);
+        updateRectangle();
+        g.setColor(rectangleColor);
+        g.fillRect((int)xDelta, (int)yDelta, 200, 50);
+
+        frames++;
+        if(System.currentTimeMillis() - lastCheck >= 1000) {
+            lastCheck = System.currentTimeMillis();
+            System.out.println("FPS: " + frames);
+            frames = 0;
+        }
+
+        repaint();
+    }
+
+    private void updateRectangle() {
+        xDelta += xDir;
+        if (xDelta > 400 || xDelta < 0) {
+            xDir = -xDir;
+            rectangleColor = getRandomColor();
+        }
+        yDelta += yDir;
+        if (yDelta > 400 || yDelta < 0) {
+            yDir = -yDir;
+            rectangleColor = getRandomColor();
+        }
+    }
+
+    private Color getRandomColor() {
+        return new Color(random.nextInt(256), random.nextInt(256), random.nextInt(256));
     }
 }
