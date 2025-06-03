b717fd9bf207e732692bd87a2199aafc5e115614
DawidMaron <daw.maron@gmail.com>
2025-06-03 12:41:27 +0200

    add playerSprite, animation, movement

--- src/main/java/com/maron/dawid/inputs/KeyboardInputs.java
+++ src/main/java/com/maron/dawid/inputs/KeyboardInputs.java
@@ -1,6 +1,7 @@
 package com.maron.dawid.inputs;
 
 import com.maron.dawid.main.GamePanel;
+import com.maron.dawid.utils.Constants.Direction;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -20,15 +21,19 @@ public class KeyboardInputs implements KeyListener {
         switch(e.getKeyCode()) {
             case KeyEvent.VK_W -> {
                 gamePanel.changeYDelta(-5);
+                gamePanel.setDirection(Direction.UP);
             }
             case KeyEvent.VK_S -> {
                 gamePanel.changeYDelta(5);
+                gamePanel.setDirection(Direction.DOWN);
             }
             case KeyEvent.VK_A -> {
                 gamePanel.changeXDelta(-5);
+                gamePanel.setDirection(Direction.LEFT);
             }
             case KeyEvent.VK_D -> {
                 gamePanel.changeXDelta(5);
+                gamePanel.setDirection(Direction.RIGHT);
             }
         }
         gamePanel.repaint();
@@ -37,5 +42,12 @@ public class KeyboardInputs implements KeyListener {
 
     @Override
     public void keyReleased(KeyEvent e) {
+        switch(e.getKeyCode()) {
+            case KeyEvent.VK_W -> {}
+            case KeyEvent.VK_S -> {}
+            case KeyEvent.VK_A -> {}
+            case KeyEvent.VK_D -> {}
+        }
+        gamePanel.setMoving(false);
     }
 }
--- src/main/java/com/maron/dawid/main/GamePanel.java
+++ src/main/java/com/maron/dawid/main/GamePanel.java
@@ -2,62 +2,134 @@ package com.maron.dawid.main;
 
 import com.maron.dawid.inputs.KeyboardInputs;
 import com.maron.dawid.inputs.MouseInputs;
+import com.maron.dawid.utils.Constants.Direction;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.util.Random;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static com.maron.dawid.utils.Constants.PlayerConstants.*;
 
 public class GamePanel extends JPanel {
     private MouseInputs mouseInputs;
-    private float xDelta = 100, yDelta = 100;
-    private float xDir = 0.25f, yDir = 0.25f;
-    private Color rectangleColor = new Color(50, 115, 140);
-    private Random random;
+    private float xDelta = 0, yDelta = 0;
+    private BufferedImage img;
+    private BufferedImage[][] animations;
+    private int animationTick, animationIndex, animationSpeed = 60;
+    private PlayerAction playerAction = PlayerAction.RUN;
+    private Direction playerDirection;
+    private boolean moving = false;
 
     public GamePanel() {
+        importImg();
+        loadAnimations();
+
+        setPanelSize();
         mouseInputs = new MouseInputs(this);
         addKeyListener(new KeyboardInputs(this));
         addMouseListener(mouseInputs);
         addMouseMotionListener(mouseInputs);
-        random = new Random();
     }
 
+    private void loadAnimations() {
+        int xSize = 32, ySize = 32;
+        animations = new BufferedImage[9][8];
+
+        for (int j = 0; j < animations.length; j++) {
+            for (int i = 0; i < animations[0].length; i++) {
+                animations[j][i] = img.getSubimage(i * xSize, j * ySize, xSize, ySize);
+
+            }
+        }
+
+    }
+
+    private void importImg() {
+        InputStream is = getClass().getResourceAsStream("/char_hood.png");
+
+        try {
+            img = ImageIO.read(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException ioException) {
+
+            }
+        }
+    }
+
+    private void setPanelSize() {
+        Dimension size = new Dimension(1280, 720);
+        setPreferredSize(size);
+    }
+
+    // for keyboard
     public void changeXDelta(int value) {
         this.xDelta += value;
     }
 
+    // for keyboard
     public void changeYDelta(int value) {
         this.yDelta += value;
     }
 
+    // for mouse
     public void setRectPos(int x, int y) {
         this.xDelta = x;
         this.yDelta = y;
     }
 
+    public void setDirection(Direction direction) {
+        playerDirection = direction;
+        moving = true;
+    }
+
+    public void setMoving(boolean moving) {
+        this.moving = moving;
+    }
+
+    public void setAnimation() {
+        if (moving) {
+            playerAction = PlayerAction.RUN;
+        } else {
+            playerAction = PlayerAction.IDLE;
+        }
+    }
+
+    private void updateAnimationTick() {
+        animationTick++;
+        if (animationTick >= animationSpeed) {
+            animationTick = 0;
+            animationIndex++;
+            if (animationIndex >= playerAction.getAnimationsNumber()) {
+                animationIndex = 0;
+            }
+        }
+    }
+
+    private void updatePosition() {
+        if(moving) {
+            switch(playerDirection) {
+                case LEFT -> xDelta -=5;
+                case UP -> yDelta -= 5;
+                case RIGHT -> xDelta += 5;
+                case DOWN -> yDelta += 5;
+            }
+        }
+    }
+
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
+        updateAnimationTick();
 
-        updateRectangle();
-        g.setColor(rectangleColor);
-        g.fillRect((int)xDelta, (int)yDelta, 200, 50);
-    }
-
-    private void updateRectangle() {
-        xDelta += xDir;
-        if (xDelta > 400 || xDelta < 0) {
-            xDir = -xDir;
-            rectangleColor = getRandomColor();
-        }
-        yDelta += yDir;
-        if (yDelta > 400 || yDelta < 0) {
-            yDir = -yDir;
-            rectangleColor = getRandomColor();
-        }
-    }
-
-    private Color getRandomColor() {
-        return new Color(random.nextInt(256), random.nextInt(256), random.nextInt(256));
+        setAnimation();
+        updatePosition();
+        g.drawImage(animations[playerAction.getActionIndex()][animationIndex], (int) xDelta, (int) yDelta, 120, 120, null);
     }
 }
