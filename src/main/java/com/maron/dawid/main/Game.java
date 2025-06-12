eae4ab60f428db2dfe87e5b711530188d25e9471
DawidMaron <daw.maron@gmail.com>
2025-06-12 11:17:59 +0200

    offtopic refactor

--- src/main/java/com/maron/dawid/main/Game.java
+++ src/main/java/com/maron/dawid/main/Game.java
@@ -9,6 +9,7 @@ public class Game implements Runnable {
 
     private final int FPS_SET = 120;
     private final int UPS_SET = 200;
+    private final static int CHARACTER_SIZE = 50;
     public final static int TILES_DEFAULT_SIZE = 32;
     public final static float SCALE = 1f;
     public final static int TILES_IN_WIDTH = 26;
@@ -34,8 +35,8 @@ public class Game implements Runnable {
     }
 
     private void initClasses() {
-        player = new Player(200, 200, (int)(50 * SCALE), (int)(50 * SCALE));
         levelHandler = new LevelHandler(this);
+        player = new Player(200, 200, (int)(CHARACTER_SIZE * SCALE), (int)(CHARACTER_SIZE * SCALE));
     }
 
     private void update() {
